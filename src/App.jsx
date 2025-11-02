8c60dbc864599a276c66c2fe81b7772408916183
jutamas051147 <jutamas.051147@gmail.com>
2025-11-02 14:35:49 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import UploadPortfolio from "./pages/UploadPortfolio";
-import Lobby from "./pages/Lobby";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Lobby />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/upload" element={<UploadPortfolio />} />
     </Routes>
   );
