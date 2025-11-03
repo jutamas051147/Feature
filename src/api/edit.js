d412e918572d26cdba704ab3a7b1d6a5021aa1de
jutamas051147 <jutamas.051147@gmail.com>
2025-11-03 21:53:07 +0700

    Update edit.js

--- src/api/edit.js
+++ src/api/edit.js
@@ -1,5 +1,5 @@
 // src/api/edit.js
-const BASE = import.meta.env.VITE_API_BASE;
+const BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:3000";
 
 export async function editPortfolio(id, formData) {
   const res = await fetch(`${BASE}/api/portfolio/${id}`, {
